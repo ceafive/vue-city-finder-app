19d9c637a013134dac7605478d6dfe8d67ed0f85
Eyram <eyram@Eyrams-MacBook-Pro.local>
2020-01-04 11:04:54 +0000

    ui refactored

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-wrap justify-between items-center my-2">
-    <div class="sm:w-full lg:w-1/3">
+    <div class="flex sm:w-full lg:w-1/3 sm:justify-center lg:justify-start">
       <span class="font-thin sm:text-sm lg:text-3xl">City Finder App</span>
     </div>
     <div
-      class="flex flex-wrap sm:w-full lg:w-2/3 lg:justify-end items-center sm:text-xs lg:text-base"
+      class="flex flex-wrap lg:w-2/3 lg:justify-end items-center sm:text-xs lg:text-base"
     >
       <span>
         IP: <span class="text-green-500 mr-1">{{ ipData.ip }}</span>
