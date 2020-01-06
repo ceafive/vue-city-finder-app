96e5969496fdc456457d4bd51f6f48973144a16c
Eyram <eyram@Eyrams-MacBook-Pro.local>
2020-01-06 12:20:54 +0000

    header component edited

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,25 +3,21 @@
     <div class="flex sm:w-full lg:w-1/3 sm:justify-center lg:justify-start">
       <span class="font-thin sm:text-sm lg:text-3xl">City Finder App</span>
     </div>
-    <div
-      class="flex flex-wrap lg:w-2/3 lg:justify-end items-center sm:text-xs lg:text-base"
-    >
+    <div class="flex flex-wrap lg:w-2/3 lg:justify-end items-center sm:text-xs lg:text-base">
       <span>
-        IP: <span class="text-green-500 mr-1">{{ ipData.ip }}</span>
+        IP:
+        <span class="text-green-500 mr-1">{{ ipData.ip }}</span>
       </span>
-      <span
-        >ISP: <span class="text-green-500 mr-1">{{ ipData.isp }}</span></span
-      >
+      <span>
+        ISP:
+        <span class="text-green-500 mr-1">{{ ipData.isp }}</span>
+      </span>
 
-      <span
-        >Country:
-        <span class="text-green-500 mr-1">{{ ipData.country_name }}</span></span
-      >
-      <img
-        class="inline h-4"
-        :src="ipData.country_flag"
-        :alt="ipData.country_name"
-      />
+      <span>
+        Country:
+        <span class="text-green-500 mr-1">{{ ipData.country_name }}</span>
+      </span>
+      <img class="inline h-4" :src="ipData.country_flag" :alt="ipData.country_name" />
     </div>
   </div>
 </template>
@@ -30,7 +26,7 @@
 export default {
   data() {
     return {
-      apiKey: "afb1b8fb45474d1f99275ae65e30037e"
+      apiKey: process.env.VUE_APP_APIKEY
     };
   },
   computed: {
