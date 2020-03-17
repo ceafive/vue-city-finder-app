04eab231dcedb73906497c95ce380f67e0b7db35
ceafive <ceafive@gmail.com>
2020-03-17 19:36:51 +0000

    transition added to CityDetails

--- src/components/CityDetails.vue
+++ src/components/CityDetails.vue
@@ -1,16 +1,18 @@
 <template>
-  <div
-    v-if="cityData"
-    class="flex flex-wrap w-full bg-appRed1 items-center text-center sm:mx-4 lg:mx-40 py-4 mb-20 lg:leading-loose"
-  >
+  <transition name="fade" mode="out-in">
     <div
-      class="w-full font-semibold sm:text-4xl lg:text-6xl"
-    >{{ cityData["places"][0]["place name"] }}</div>
-    <div class="w-full text-2xl">{{ cityData["places"][0]["state"] }}</div>
-    <div class="w-full text-2xl">{{ cityData["places"][0]["state abbreviation"] }}</div>
-    <div class="w-full text-2xl">{{ cityData["post code"] }}</div>
-    <div class="w-full text-2xl">{{ cityData["country"] }}</div>
-  </div>
+      v-if="cityData"
+      class="flex flex-wrap w-full bg-appRed1 items-center text-center sm:mx-4 lg:mx-40 py-4 mb-20 lg:leading-loose"
+    >
+      <div
+        class="w-full font-semibold sm:text-4xl lg:text-6xl"
+      >{{ cityData["places"][0]["place name"] }}</div>
+      <div class="w-full text-2xl">{{ cityData["places"][0]["state"] }}</div>
+      <div class="w-full text-2xl">{{ cityData["places"][0]["state abbreviation"] }}</div>
+      <div class="w-full text-2xl">{{ cityData["post code"] }}</div>
+      <div class="w-full text-2xl">{{ cityData["country"] }}</div>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -22,5 +24,17 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
 
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import i18n from 'vue-i18n'
 import "./assets/css/styles.css"
-
 
 Vue.config.productionTip = false;
 
